c74806db3b2ca112128588ff11f5e941cf1520d2
mixiplayzzz <nkanade023@gmail.com>
2026-07-22 20:26:31 +0530

    Remove main check and run app directly

--- app.py
+++ app.py
@@ -38,6 +38,3 @@ def sensitivity_gallery(level):
     return render_template("sensitivity_gallery.html", level=level.capitalize())
 
 
-if __name__ == "__main__":
-    import os
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
